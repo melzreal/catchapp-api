c9303e2b0349bf1d0e76a96e18ae3b6302608ab5
melzreal <melissa.real@hotmail.com>
2019-09-28 14:51:12 +0100

    added seeds to db:

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Catchfriend.create(friendname: "Popo", contact: "mipo12223@gmail.com", reached_out: false)
+Catchfriend.create(friendname: "Shinji", contact: "shinjikun22@gmail.com", reached_out: false)
+Catchfriend.create(friendname: "Tanaka", contact: "tetekaki2@gmail.com", reached_out: false)
+Catchfriend.create(friendname: "Polianne", contact: "polipoti29494@gmail.com", reached_out: false)
